cfa7a6b5f3e4edabcb91f9541180181cc656ef29
Alejandro <mediadocena13@gmail.com>
2019-01-10 10:42:45 +0100

    jarl2

---
+++ Salieri/src/Utiles/test.java
@@ -0,0 +1,12 @@
+package Utiles;
+
+public class test {
+
+	public static void main(String[] args) {
+		Salieri ex = new Salieri();
+		String a = "JUAN";
+		System.out.println(ex.invertirString(a));
+		
+	}
+
+}
