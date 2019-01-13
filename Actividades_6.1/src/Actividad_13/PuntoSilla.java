46a8e9391b25f6ea1b0c0cf0e278155919bf0bb9
mediadocena <45419684+mediadocena@users.noreply.github.com>
2019-01-13 15:13:44 +0100

    Ej 13 empezado

---
+++ Actividades_6.1/src/Actividad_13/PuntoSilla.java
@@ -0,0 +1,25 @@
+package Actividad_13;
+
+public class PuntoSilla {
+
+	public static void main(String[] args) {
+		int[][] silla=new int[4][4];
+		int sillant=0;
+		for (int i = 0; i < silla.length; i++) {
+			for (int j = 0; j<silla[0].length; j++) {
+				silla [i][j]=(int)(Math.random()*101);
+				System.out.print(silla[i][j]+" ");
+			}
+			System.out.println("");
+		}
+		for (int i = 0; i < silla.length; i++) {
+			
+			if(silla[i][0]>sillant) {
+				sillant=silla[i][0];
+			}
+			
+		}
+		
+	}
+
+}
