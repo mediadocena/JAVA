88f0a76982b76cd7a50d158244f89b8d12a4b93a
Alejandro <mediadocena13@gmail.com>
2019-01-14 11:03:09 +0100

    Actividad 14 donete

---
+++ Actividades_6.1/src/Actividad_14/Factorial.java
@@ -0,0 +1,17 @@
+package Actividad_14;
+
+public class Factorial {
+	int x;
+	int y=x-1;
+	Factorial(){
+	
+	}
+	public int Factor(int x) {
+		y=x-1;
+		if(y==1) {
+			return x;
+		}else {
+			return x*Factor(x-1);
+		}
+	}
+}
---
+++ Actividades_6.1/src/Actividad_17/Caracol.java
@@ -0,0 +1,14 @@
+package Actividad_17;
+
+import Utiles.Salieri;
+
+public class Caracol {
+
+	public static void main(String[] args) {
+		Salieri ex = new Salieri();
+		int n = ex.controlaentero();
+		int[][] array = new int[n][n];
+		
+	}
+
+}
