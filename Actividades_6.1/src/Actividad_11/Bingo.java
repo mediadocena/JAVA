48aea2f23393a07b7e90a4f6fcbd81cd88b2ac3f
mediadocena <45419684+mediadocena@users.noreply.github.com>
2019-01-16 21:02:41 +0100

    FINAL

--- Actividades_6.1/src/Actividad_11/Bingo.java
+++ Actividades_6.1/src/Actividad_11/Bingo.java
@@ -3,66 +3,71 @@ package Actividad_11;
 public class Bingo {
 
 	public static void main(String[] args) {
-		int[][]numplayer = new int[5][5];
-		int[][]numplayer2 = new int[5][5];
-		int numa = 0;
-		boolean bingo = false;
-		boolean bingo2 = false;
-		//Generamos los números del jugador
-		//Generamos las filas y columnas:
-		System.out.println("Estos son tus números:");
-		for (int i = 0; i < 5; i++) {
-			numplayer[i][0]=(int) (Math.random()*101);
-			numplayer2[i][0]=(int) (Math.random()*101);
-			System.out.print(numplayer[i][0]);
-			System.out.print(" ");
-			for (int j = 0; j < 5; j++) {
-				numplayer[0][j]=(int) (Math.random()*101);
-				numplayer2[0][j]=(int) (Math.random()*101);
-				System.out.print(numplayer[0][j]);
-				
-				System.out.print(" ");
-			}
-			
-			System.out.println("");
+		int[] carton1=new int[15];
+		int[] carton2=new int[15];
+		boolean gana1=false;
+		boolean gana2=false;
+		//Asignamos números a los cartones:
+		System.out.println("Cartones:");
+		for (int i = 0; i < carton1.length; i++) {
+			carton1[i] = (int)(Math.random()*89+1);
+			carton2[i] =(int)(Math.random()*89+1);
 		}
+		//Imprimimos los cartones:
+		System.out.println("Carton 1:");
+		for (int i = 0; i < carton2.length; i++) {
+		System.out.print(carton1[i]+" ");
+		}
+		System.out.println();
+		System.out.println("Carton 2:");
+		for (int i = 0; i < carton2.length; i++) {
+			System.out.print(carton2[i]+" ");
+		}
+		System.out.println();
 		do {
-			numa=(int)(Math.random()*101);
-			//Marcamos los números correctos
-			for (int i = 0; i < 5; i++) {
-				if(numplayer[0][i]==numa) {
-					numplayer[0][i]=-1;
-				}
-				if(numplayer2[0][i]==numa) {
-					numplayer2[0][i]=-1;
-				}
-				if(numplayer[i][0]==numa) {
-					numplayer[i][0]=-1;
-				} 
-				if(numplayer2[i][0]==numa){
-					numplayer2[i][0]=-1;
+			int n= (int)(Math.random()*89+1);
+			//Comprobamos jugador 1
+			for (int i = 0; i < carton1.length; i++) {
+				if(carton1[i]==n) {
+					carton1[i]=0;
 				}
 			}
-			for (int i = 0; i < 5; i++) {
-				if(numplayer[i][0]==-1) {
-					bingo=true;
-				}else {bingo=false;break;}
-				if(numplayer2[i][0]==-1) {
-					bingo2=true; 
-					}else {bingo2=false;break;}
-				if(numplayer[0][i]==-1) {
-					bingo=true;
-				}else {bingo=false;break;}
-					if(numplayer2[0][i]==-1) {
-					bingo2=true; 
-					}else {bingo2=false;break;}
+			//Comprobamos jugador 2
+			for (int i = 0; i < carton2.length; i++) {
+				if(carton2[i]==n) {
+					carton2[i]=0;
+				}
 			}
-			
-		}while(bingo == false && bingo2==false);
-		if(bingo=true) {
+			for (int i = 0; i < carton1.length; i++) {
+				if(carton1[i]==0) {
+					gana1 = true;
+				}else {
+					gana1=false;
+					break;
+				}
+			}
+			for (int i = 0; i < carton2.length; i++) {
+				if(carton2[i]==0) {
+					gana2 = true;
+				}else {
+					gana2=false;
+					break;
+				}
+			}
+		} while (gana1==false||gana2==false);
+		//Imprimimos los cartones:
+				System.out.println("Carton 1:");
+				for (int i = 0; i < carton2.length; i++) {
+				System.out.print(carton1[i]+" ");
+				}
+				System.out.println();
+				System.out.println("Carton 2:");
+				for (int i = 0; i < carton2.length; i++) {
+					System.out.print(carton2[i]+" ");
+				}
+				System.out.println();
+		if(gana1==true) {
 			System.out.println("Gana el jugador 1");
-			
-		}else {System.out.println("Gana el jugador 2");}
+		}else if(gana2==true){System.out.println("Gana el jugador 2");}
 	}
-
 }
