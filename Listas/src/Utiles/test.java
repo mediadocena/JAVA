698a3d37fea281f3d14f7a98e6ecca51a10b47cd
mediadocena <45419684+mediadocena@users.noreply.github.com>
2019-01-28 20:36:47 +0100

    janser2

---
+++ Listas/src/Utiles/test.java
@@ -0,0 +1,20 @@
+package Utiles;
+
+public class test {
+
+	public static void main(String[] args) {
+		Salieri ex = new Salieri();
+		String a = "JUAN";
+		int cap = 1111;
+		int[] array=new int[12];
+		/*System.out.println(ex.invertirString(a));
+		System.out.println(ex.esCapicua(cap));
+		ex.controladouble();
+		ex.controlaentero();
+		ex.controlanegativo();
+		ex.controlapositivo();*/
+		ex.vectorUtil(array, 1);
+		ex.vectorUtil(array, 2);
+	}
+
+}
