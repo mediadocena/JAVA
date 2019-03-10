c9833e1b48c856571b802c8fa8d590b7bd6da950
mediadocena <45419684+mediadocena@users.noreply.github.com>
2019-03-10 16:07:33 +0100

    ejercicio 6 (errores)

---
+++ Actividades_7.1/src/Ej6/Test.java
@@ -0,0 +1,18 @@
+package Ej6;
+
+import Utiles.Salieri;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Catalogo g = new Catalogo();
+		Salieri ex = new Salieri();
+		g.anñadeObra();
+		g.buscarObra(ex.controlaenteroSt("Introduzca el nº de inventario a buscar"));
+		g.eliminaObra(ex.controlaenteroSt("Introduzca el nº de inventario a borrar"));
+		g.Superficie();
+		g.masAlta();
+
+	}
+
+}
